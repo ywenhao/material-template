a14e39bc3d327136dd9610ac11e2182e27350e69
ywenhao <actions.win@foxmail.com>
2025-02-25 19:50:18 +0800

    fix: gulp

--- internal/build-task/task/tsconfig.js
+++ internal/build-task/task/tsconfig.js
@@ -3,5 +3,5 @@ import { join } from 'node:path'
 import { distPath } from '../utils/path.js'
 
 export async function cleanTsBuildinfo() {
-  return deleteAsync(join(distPath, 'tsconfig.*.tsbuildinfo'), { force: true })
+  return deleteAsync(join(distPath, '*.tsbuildinfo'), { force: true })
 }
