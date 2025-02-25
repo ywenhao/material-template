10fd947c242f80467defd026e4c79b00b234efc0
ywenhao <actions.win@foxmail.com>
2025-02-25 21:20:37 +0800

    fix: gulp

--- internal/build-task/task/replace.js
+++ internal/build-task/task/replace.js
@@ -1,14 +1,13 @@
 import { readFile, writeFile } from 'node:fs/promises'
-import { join } from 'node:path'
 import { glob } from 'tinyglobby'
 import { At_PKG_NAME, PKG_NAME, distPath } from '../utils/path.js'
 
 export async function replaceAlias() {
-  const files = await glob(join(distPath, '**/*.ts'), { deep: true })
+  const files = await glob('**/*.ts', { cwd: distPath, deep: true, absolute: true, onlyFiles: true })
   const promises = files.map(async (path) => {
     const content = await readFile(path, 'utf-8', { cwd: process.cwd() })
     if (!content.includes(At_PKG_NAME)) return
-    const newContent = content.replace(new RegExp(`from '${At_PKG_NAME}`, 'g'), `from '${PKG_NAME}`)
+    const newContent = content.replace(new RegExp(`'${At_PKG_NAME}`, 'g'), `'${PKG_NAME}`)
     await writeFile(path, newContent, 'utf-8')
   })
 
