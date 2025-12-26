79aa57006cb3b9ef3b97f0935de93293494bbae4
yuanwenhao <actions.win@foxmail.com>
2025-12-26 16:56:04 +0800

    chore: update deps

--- internal/build-utils/src/pkg.ts
+++ internal/build-utils/src/pkg.ts
@@ -1,11 +1,11 @@
-import { PKG_NAME } from '@material-template/build-constants'
-import { findWorkspacePackages } from '@pnpm/workspace.find-packages'
+// import { PKG_NAME } from '@material-template/build-constants'
+// import { findWorkspacePackages } from '@pnpm/workspace.find-packages'
 import { resolve } from 'node:path'
 
 export const getPath = (path: string) => resolve(process.cwd(), path)
 
-export async function getMainPrefix() {
-  const list = await findWorkspacePackages(getPath('./packages'))
-  const item = list.find((item) => item.manifest?.name === PKG_NAME)
-  return item?.rootDir.split(/(\/packages\/|\\packages\\)/).pop()
-}
+// export async function getMainPrefix() {
+//   const list = await findWorkspacePackages(getPath('./packages'))
+//   const item = list.find((item) => item.manifest?.name === PKG_NAME)
+//   return item?.rootDir.split(/(\/packages\/|\\packages\\)/).pop()
+// }
